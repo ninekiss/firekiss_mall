47a80bacaa53878de68986c0ee4d249906117192
perloliu <aodocoo@gmail.com>
2019-01-30 05:18:45 +0800

    修复了一些bug

--- firekiss/apps/goods/admin.py
+++ firekiss/apps/goods/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from goods.models import GoodsSKU
 
 # Register your models here.
+admin.site.register(GoodsSKU)
--- firekiss/apps/goods/urls.py
+++ firekiss/apps/goods/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
 from goods import views
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='index'),
 ]
